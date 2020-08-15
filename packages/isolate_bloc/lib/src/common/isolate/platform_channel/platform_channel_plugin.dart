7fb73fb618404ad07099149befd92374b5eb158e
Zemlaynikin Max <zemlyanikin.max@yandex.ru>
2020-08-15 20:02:49 +0300

    Add docs for Plugin class

--- packages/isolate_bloc/lib/src/common/isolate/platform_channel/platform_channel_plugin.dart
+++ packages/isolate_bloc/lib/src/common/isolate/platform_channel/platform_channel_plugin.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/foundation.dart';
 
+/// Library info for [PlatformChannelSetup]. 
 class Plugin {
   final String name;
+
+  /// MethodChannel names that use this plugin. 
   final List<String> methodChannels;
 
   Plugin({
